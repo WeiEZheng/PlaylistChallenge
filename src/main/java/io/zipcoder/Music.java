c13dc2a0e56c5a5a70037442896abb014c869444
Wei <wei@Zipcoders-MacBook-Pro.local>
2022-03-08 17:52:10 -0500

    finished playlist

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -1,5 +1,8 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Music {
 
     private String[] playList;
@@ -9,6 +12,32 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        int forwardMethod = playList.length, backwardMethod = playList.length, temp=0;
+        List<Integer> endingIndex= new ArrayList<Integer>();
+        //for multiple copies of q
+        for (int i=0; i< playList.length;i++){
+            if (playList[i].equals(selection)) {
+                endingIndex.add(i);
+            }
+        }
+        for (Integer s: endingIndex) {
+            if (s > startIndex) {
+                temp=s-startIndex;
+                if (temp<forwardMethod)
+                    forwardMethod = temp;
+                temp=startIndex + (playList.length - s);
+                if (temp<backwardMethod)
+                    backwardMethod = temp;
+            }
+            if (s < startIndex){
+                temp= (playList.length-startIndex)+s;
+                if (temp<forwardMethod)
+                    forwardMethod = temp;
+                temp=startIndex-s;
+                if (temp<backwardMethod)
+                    backwardMethod = temp;
+            }
+        }
+        return forwardMethod<backwardMethod?forwardMethod:backwardMethod;
     }
 }
